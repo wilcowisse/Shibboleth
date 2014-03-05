3c40e96e425ba082a8345c91e4887c68a5f716a0
Wilco Wisse <wilcowisse@server.fake>
2014-03-05 15:01:32 +0100

    prepared actions to be used as api

--- db/chunksofuser.sql
+++
@@ -1,7 +0,0 @@
-SELECT Chunks.'start', Chunks.'end', Chunks.'time', Committers.email, Committers.name, Files.repo, Files.file_path, Files.head 
-FROM RecordLinks 
-JOIN Committers ON RecordLinks.committer=Committers.id 
-JOIN Chunks ON Committers.id=Chunks.committer_id 
-JOIN Files ON Chunks.file_id=Files.id 
-WHERE RecordLinks.user='creationix'
-ORDER BY Files.file_path, Chunks.start;
--- db/usersoffile.sql
+++
@@ -1,6 +0,0 @@
-SELECT RecordLinks.user, Chunks.'start', Chunks.'end', Chunks.'time', Committers.email, Committers.name, Files.repo, Files.file_path, Files.head 
-FROM RecordLinks 
-JOIN Committers ON RecordLinks.committer=Committers.id 
-JOIN Chunks ON Committers.id=Chunks.committer_id 
-JOIN Files ON Chunks.file_id=Files.id 
-WHERE Files.id=6;
